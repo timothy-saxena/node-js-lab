c24183d155c765ab11fa30e029f1c1f824d04bdc
Timothy <timothy.saxena@gmail.com>
2026-03-05 02:19:31 +0530

    updating validation Js

--- mid1-expected/q2-script.js
+++ mid1-expected/q2-script.js
@@ -5,26 +5,46 @@ function validate() {
     var contact = document.getElementById("contact").value;
     var address = document.getElementById("address").value;
 
-    if (username == "") {
-        alert("Enter username");
+    // Username validation
+    if (username.length < 3) {
+        alert("Username must be at least 3 characters");
         return false;
     }
 
-    if (password == "") {
-        alert("Enter password");
+    // Password validation
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters");
         return false;
     }
 
-    if (email == "") {
-        alert("Enter email");
+    if (password == password.toLowerCase()) {
+        alert("Password must contain at least one capital letter");
         return false;
     }
 
-    if (contact == "") {
-        alert("Enter contact number");
+    if (
+        !password.includes("@") &&
+        !password.includes("#") &&
+        !password.includes("$") &&
+        !password.includes("!")
+    ) {
+        alert("Password must contain at least one special character");
         return false;
     }
 
+    // Email validation
+    if (!email.includes("@")) {
+        alert("Email must contain @");
+        return false;
+    }
+
+    // Contact validation
+    if (contact.length != 10) {
+        alert("Contact number must be 10 digits");
+        return false;
+    }
+
+    // Address validation
     if (address == "") {
         alert("Enter address");
         return false;
